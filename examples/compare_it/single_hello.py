41b04a2589c7e2db89484e658c567ac7ad0b26d4
Emirhan Uçan <semaemirhan555@gmail.com>
2026-09-17 15:13:43 +0300

    compare it!

---
+++ examples/compare_it/single_hello.py
@@ -0,0 +1,32 @@
+"""
+Module: single_hello
+Type: <class 'module'>
+
+File: built-in
+
+"""
+
+# Package: 
+# Spec: ModuleSpec(name='single_hello', loader=<class '_frozen_importlib.BuiltinImporter'>, origin='built-in')
+
+
+def factorial(n: 'int') -> 'int':
+    # COMPILED CODE - Metadata extraction:
+    # Local variables:
+    #   n
+    #   result
+    #   i
+    # Argument count: 1
+    # Keyword-only args: 0
+    # Local variables count: 3
+    # AG: STUB_IDENTIFIED - Compiled code - no Python bytecode available
+
+
+def main() -> 'int':
+    # COMPILED CODE - Metadata extraction:
+    # Local variables:
+    #   cmd
+    # Argument count: 0
+    # Keyword-only args: 0
+    # Local variables count: 1
+    # AG: STUB_IDENTIFIED - Compiled code - no Python bytecode available
